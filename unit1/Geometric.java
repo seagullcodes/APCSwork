b94cfff3e46ae616fbf34e575ae04fdec1bfdc98
seagullcodes <113067874+seagullcodes@users.noreply.github.com>
2022-09-13 18:57:53 -0700

    Unit 1 First Assignment Set

---
+++ unit1/Geometric.java
@@ -0,0 +1,12 @@
+public class Geometric
+{
+  public static void main(String[] args)
+  {
+
+    for int i = 0; i<20; i += i+=2)
+    {
+      System.out.println(i);
+      i = i*i;
+    }
+  }
+}
---
+++ unit1/MultiplesOf3.java
@@ -0,0 +1,12 @@
+public class MultiplesOf3
+{
+  public static void main(String[] args)
+  {
+    int m = 3;
+    for(int i = 0;i<10; i+=m)
+    {
+      System.out.println(i);
+    }
+  }
+
+}
