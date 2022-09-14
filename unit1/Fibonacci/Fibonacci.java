46465ab78f69bf8a98dc8719736121c92455ea5f
seagullcodes <113067874+seagullcodes@users.noreply.github.com>
2022-09-13 19:20:53 -0700

    Fibonacci Update

---
+++ unit1/Fibonacci/Fibonacci.java
@@ -0,0 +1,27 @@
+public class Fibonacci
+{
+  public static void main(String[] args)
+  {
+    int a = 1;
+    int b = 1;
+    int c = a+b;
+
+        System.out.println(a);
+      a = b+c;
+      System.out.println(b);
+      b = a+c;
+      System.out.println(c);
+      c = a+b;
+      System.out.println(a);
+      a = b+c;
+      System.out.println(b);
+      b = a+c;
+      System.out.println(c);
+      c = a+b;
+      
+
+
+
+
+  }
+}
--- unit1/Geometric.java
+++ unit1/Geometric.java
@@ -3,7 +3,7 @@ public class Geometric
   public static void main(String[] args)
   {
 
-    for int i = 0; i<20; i += i+=2)
+    for (int i = 0; i<20; i += i+=2)
     {
       System.out.println(i);
       i = i*i;
