ed7a3a0a5ad8bef594b0eca5b4c06e6b4c766c45
seagullcodes <113067874+seagullcodes@users.noreply.github.com>
2022-09-25 11:04:21 -0700

    Update

--- Unit2/Icyhot.java
+++ Unit2/Icyhot.java
@@ -1,5 +1,17 @@
 public class Icyhot
 {
+  public static void testIcyhot(int temp1, int temp2, boolean expected){
+  boolean result = temp(temp1, temp2);
+  System.out.println("temp. 1: " + temp1 + "temp. 2:" + temp2 + boolean expected);
+
+  if(result == expected)
+  {
+    System.out.println( " happy");
+  }
+  else{System.out.println("sad");
+}
+}
+
 public static boolean temp(int temp1, int temp2)
  {
    if(temp1 < 0 && temp2>100 || temp1 > 100 & temp2 < 100)
@@ -15,5 +27,6 @@ public static boolean temp(int temp1, int temp2)
   {
     System.out.println(temp(72,2));
     System.out.println(temp(-2, 110));
+    System.out.println(testIcyhot(-5, 240, true));
   }
 }
