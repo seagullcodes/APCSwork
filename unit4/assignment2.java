25ac94adcf403ff180428a4c07bfbf07cbf1d610
seagullcodes <113067874+seagullcodes@users.noreply.github.com>
2022-10-08 13:07:16 -0700

    done!

--- unit4/assignment2.java
+++ unit4/assignment2.java
@@ -1,7 +1,6 @@
 
 public class assignment2
 {
-
     public static int sumofsquares(int b)//3
     {
       int result = 0;
@@ -13,46 +12,84 @@ public class assignment2
       }
       return result;
     }
-  /*  public static int countOccurences(String a, String ch)
+    public static boolean testSumofsquares(int b, int expected)
     {
-      int result = 0;
-      int diff = 0;
-
-      if(a.length() % 3 == 0){
-        diff = 0;
-      } else if(a.length() % 3 == 1){
-        diff = 1;
-      } else if(a.length() % 3 == 2){
-        diff = 2;
-      }
-
-      for(int l = 0; l>a.length()-diff; l++)
+      int result = (sumofsquares(b));
+      System.out.println("Int " + b + "expected " + expected + "result " + result);
+      if(result == expected)
       {
-        String b = a.substring(l, l+3);
-        if(b.equals(ch))
-        {
-
+        return true;
       }
-      return result;
+      else{
+        return false;
+      }
+
     }
-  }*/
+   public static int countOccurences(String a, String ch)
+    {
+      int h = ch.length();
+      int d = 0;
+
+      for(int i = 0; i+h<=a.length(); i++)
+      {
+        String sub = a.substring(0+i,h+i);
+        if(sub.equals(ch))
+        {
+          d+=1;
+        }
+        else
+        {
+          d += 0;
+        }
+      }
+      return d;
+  }
+  public static boolean testCountOccurences(String a, String ch, int expected)
+  {
+    int result = (countOccurences(a, ch));
+    System.out.println("String " + a + "expected " + expected + "result " + result);
+    if(result == expected)
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
+
+  }
     public static String reverse(String start)
     {
       String reverseresult = " ";
 
-      for(int r = 0; r>start.length(); r++)
+      for(int r = 0; r<start.length(); r++)
       {
         String ra = start.substring(start.length()-1-r, start.length()-r);
         reverseresult = reverseresult + ra;
       }
       return reverseresult;
     }
+    public static String testreverse(String start, String expected)
+    {
+      String result = reverse(start);
+
+      System.out.println("String " + start + "expected " + expected + "result " + result);
+      if(result.equals(expected))
+      {
+        System.out.println("working!");
+      }
+      else{
+        System.out.println("not working");
+      }
+
+      return "";
+
+    }
 
     public static int factorial(int f)
     {
       int m = 1;
 
-      for(int i = 1; i <= f+1; i++)
+      for(int i = 1; i <= f; i++)
       {
         m = i*m;
 
@@ -60,13 +97,43 @@ public class assignment2
 
       return m;
     }
+    public static String testFactorial(int f, int expected)
+    {
+      int result = (factorial(f));
+      System.out.println("Int " + f + "expected " + expected + "result " + result);
+      if(result == expected)
+      {
+        return "working!";
+      }
+      else{
+        return "not working!";
+      }
+
+    }
 
     public static void main(String[] args)
     {
-      System.out.println(sumofsquares(3)+" Expect14");
-      //System.out.println(countOccurences("Mississippi", "iss")+" expect2");
-      String start = "bad";
-      System.out.println(reverse(start)+"expect dab");
-      System.out.println(factorial(4)+"Expect24");
+      System.out.println(testSumofsquares(1,1));
+      System.out.println(testSumofsquares(2,5));
+      System.out.println(testSumofsquares(3,14));
+      System.out.println(testSumofsquares(4,30));
+      System.out.println(testSumofsquares(5,55));
+      System.out.println(testCountOccurences("Mississippi", "iss", 2));
+      System.out.println(testCountOccurences("banananana", "an", 4));
+      System.out.println(testCountOccurences("sarah", "a", 2));
+      System.out.println(testCountOccurences("yoyoyoyohejyo", "yo", 5));
+      System.out.println(testreverse("bad", " dab"));
+      System.out.println(testreverse("hi", " ih"));
+      System.out.println(testreverse("sarah", " haras"));
+      System.out.println(testreverse("Hello, world!", " !dlrow ,olleH"));
+      System.out.println(testreverse("tacocat", " tacocat"));
+      System.out.print(testFactorial(0,1));
+      System.out.print(testFactorial(1,1));
+      System.out.print(testFactorial(2,2));
+      System.out.print(testFactorial(3,6));
+      System.out.print(testFactorial(4,24));
+      System.out.print(testFactorial(5,120));
+      System.out.print(testFactorial(6,720));
+      System.out.print(testFactorial(7,5040));
     }
 }
